1d63223c4b1efd6af4086a3d969b7c1a34abda4e
Steve Bogdan <45405856+sbogdan60@users.noreply.github.com>
2019-03-09 11:03:03 -0800

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-census1_api_key = "bdb18cf93fde53cedda10d62ee24bc6d29e147e5"
-census2_api_key = "e4802ddb92bf7b297f5f10de4176b9d5341c10c8"
-weather_api_key = "13d1cf66a30cab63715a43a5d383e7df"
-gkey = "AIzaSyAbhFiP9JQbFBbhxVcHmr9QqZEJyRMkF0s"
